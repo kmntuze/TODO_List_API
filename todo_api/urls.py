77c43c57bfe591aa18af69309068ac0a2537b071
Kamohelo Mntuze <kamomntuze@outlook.com>
2026-05-11 17:20:41 +0200

    Add root redirect

--- todo_api/urls.py
+++ todo_api/urls.py
@@ -1,10 +1,8 @@
 from django.shortcuts import redirect
 from django.urls import include, path
 
-
 def root_redirect(request):
     return redirect('/api/todos/')
-
 
 urlpatterns = [
     path('', root_redirect),
